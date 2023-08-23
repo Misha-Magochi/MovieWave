83a3e063ef6f83258237a4722a912893f9edaa7c
Misha Magochi <magochimisha@gmail.com>
2023-08-23 11:40:51 +0300

    connected links to four menus and icons

--- src/components/lib/axios.ts
+++ src/components/lib/axios.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 
 const axiosinstance = axios.create({
+  /* baseURL: process.env.API_URL,  */
   baseURL: 'http://localhost:8080', 
   
 });
--- src/components/side-bar/Side-bar.tsx
+++ src/components/side-bar/Side-bar.tsx
@@ -1,33 +1,11 @@
-import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
-import type { MenuProps, SiderProps } from 'antd';
+import type { SiderProps } from 'antd';
 import { Layout, Menu, theme } from 'antd';
 import axiosinstans from '../lib/axios';
 import React, {useState, useEffect} from 'react';
 import { AxiosResponse } from 'axios';
-import expressServer from '../lib/axios'; 
+
 
 const { Sider } = Layout;
-
-
-
-const items2: MenuProps['items'] = [UserOutlined, LaptopOutlined, NotificationOutlined].map(
-  (icon, index) => {
-    const key = String(index + 1);
-
-    return {
-      key: `sub${key}`,
-      icon: React.createElement(icon),
-      label: `sidenav ${key}`,
-      children: new Array(4).fill(null).map((_, j) => {
-        const subKey = index * 4 + j + 1;
-        return {
-          key: subKey,
-          label: `option${subKey}`,
-        };
-      }),
-    };
-  },
-);
 
 const Sidebar: React.FC<SiderProps> = (props) => {
   const {
@@ -37,16 +15,29 @@ const Sidebar: React.FC<SiderProps> = (props) => {
   const [sidebarData, setSidebarData] = useState([]);
 
   useEffect(() => {
-    axiosinstans.get('/')
+    axiosinstans.get('/api/siderbar-link')
       .then((response: AxiosResponse) => {
         const responseData = response.data;
-        console.log(responseData);
         setSidebarData(responseData);
       })
       .catch((error: any) => {
         console.error('Error fetching data:', error);
       });
   }, []);
+
+  const menuItems = sidebarData.map((dataItem: any, index: number) => {
+    return {
+      key:  `sub${index + 1}`,
+      icon: <img src={dataItem.iconPath} alt={`Icon ${index}`} />,
+      label: `sidenav ${index + 1}`,
+      children: dataItem.links.map((link: any, j: number) => {
+        return {
+          key: `${dataItem.type}${j}`,
+          label: link.label,
+        };
+      }),
+    }
+  })
 
   return (
     <Sider style={{ background: colorBgContainer }} width={200}>
@@ -56,7 +47,7 @@ const Sidebar: React.FC<SiderProps> = (props) => {
         defaultSelectedKeys={['1']}
         defaultOpenKeys={['sub1']}
         style={{ height: '100%' }}
-        items={items2}
+        items={menuItems}
       />
     </Sider>
   );
