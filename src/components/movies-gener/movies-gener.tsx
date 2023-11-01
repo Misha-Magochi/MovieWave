c4cb104ce1dd66ade7eaf746a7f25d470ed156a6
Misha Magochi <magochimisha@gmail.com>
2023-11-01 18:36:08 +0300

    fix-bag whit movieByGenre

--- src/components/movies-gener/movies-gener.tsx
+++ src/components/movies-gener/movies-gener.tsx
@@ -16,16 +16,12 @@ function MoviesGener() {
                 const response = await axiosinstans.get('/api/routing-movies', {
                     params: {
                         movieByType: movieType,
-                        movieGener: movieGener,
                         sortedBy: 'imdbRating',
+                        movieByGenre: movieGener,
                     },
                 });
 
-                const filteredMovies = response.data.result.filter((movie) => {
-                    return movie.Genre && movie.Genre.toLowerCase().includes(movieGener.toLowerCase());
-                });
-
-                setMovies(filteredMovies);
+                setMovies(response.data.result);
                 setLoading(false);
             } catch (error) {
                 console.error('Error:', error);
@@ -36,39 +32,40 @@ function MoviesGener() {
         fetchData();
     }, [movieType, movieGener]);
 
+
     return (
         <div className="movie-list">
-            {loading ? (
-                <Spinner />
-            ) : (
-                <>
-                    <div className="movie-head">
-                        <span className="movie-head-title">Watch {movieType} of {movieGener} </span>
+          {loading ? (
+            <Spinner />
+          ) : (
+            <>
+              <div className="movie-head">
+              <span className="movie-head-title">Watch {movieType} of {movieGener} </span>
+              </div>
+              <div className="movie-item-block">
+                {movies[0].map((movie) => (
+                  <div key={movie._id} className="movie-item">
+                    <img
+                      src={movie.Poster}
+                      alt={movie.Title}
+                      className="movie-image"
+                      onError={(e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+                        const target = e.target as HTMLImageElement;
+                        target.src = imgNF;
+                      }}
+                    />
+                    <h3 className="movie-title">{movie.Title}</h3>
+                    <div className="movie-rls">
+                      <span className="movie-gener">{movie.Type}</span>
+                      <div>{movie.Released}</div>
                     </div>
-                    <div className="movie-item-block">
-                        {movies.map((movie) => (
-                            <div key={movie._id} className="movie-item">
-                                <img
-                                    src={movie.Poster}
-                                    alt={movie.Title}
-                                    className="movie-image"
-                                    onError={(e: React.SyntheticEvent<HTMLImageElement, Event>) => {
-                                        const target = e.target as HTMLImageElement;
-                                        target.src = imgNF;
-                                    }}
-                                />
-                                <h3 className="movie-title">{movie.Title}</h3>
-                                <div className="movie-rls">
-                                    <span className="movie-gener">{movie.Type}</span>
-                                    <div>{movie.Released}</div>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                </>
-            )}
+                  </div>
+                ))}
+              </div>
+            </>
+          )}
         </div>
-    );
+      );
 }
 
 export default MoviesGener;
--- src/components/side-bar/Side-bar.tsx
+++ src/components/side-bar/Side-bar.tsx
@@ -3,7 +3,7 @@ import { Layout, Menu, theme } from 'antd';
 import axiosinstans from '../../lib/axios';
 import React, { useState, useEffect } from 'react';
 import { AxiosResponse } from 'axios';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const { Sider } = Layout;
 const Sidebar: React.FC<SiderProps> = (props) => {
@@ -11,47 +11,41 @@ const Sidebar: React.FC<SiderProps> = (props) => {
     token: { colorBgContainer },
   } = theme.useToken();
   const [sidebarData, setSidebarData] = useState([]);
+  const location = useLocation();
+
   useEffect(() => {
     axiosinstans.get('/api/siderbar-link')
       .then((response: AxiosResponse) => {
         const responseData = response.data;
         setSidebarData(responseData);
-        console.log(responseData);
       })
       .catch((error: any) => {
         console.error('Error fetching data:', error);
       });
   }, []);
 
-  const menuItems = sidebarData.map((dataItem: any, index: number) => {
-    return {
-      key: `sub${index + 1}`,
-      icon: <Avatar src={dataItem.iconPath} alt={dataItem.label} />,
-      label: `${dataItem.type} `,
-      children: dataItem.links.map((link: any, j: number) => {
-        return {
-          key: `${dataItem.type}${j}`,
-          label: link.label,
-          to: `${dataItem.type}/${link.path}`,
-        };
-      }),
-    };
-  });
-
   return (
     <Sider style={{ background: colorBgContainer }} width={200}>
       <Menu
         theme='dark'
         mode="inline"
-        defaultSelectedKeys={['1']}
         defaultOpenKeys={['sub1']}
         style={{ height: '100%' }}
+        selectedKeys={[location.pathname]}
       >
-        {menuItems.map((menuItem) => (
-          <Menu.SubMenu key={menuItem.key} title={menuItem.label}>
-            {menuItem.children.map((child) => (
-              <Menu.Item key={child.key}>
-                <Link to={child.to}>{child.label}</Link>
+        {sidebarData.map((dataItem: any, index: number) => (
+          <Menu.SubMenu
+            key={`sub${index + 1}`}
+            title={
+              <span>
+                <Avatar src={dataItem.iconPath} alt={dataItem.label} />
+                <span>{dataItem.type}</span>
+              </span>
+            }
+          >
+            {dataItem.links.map((link: any, j: number) => (
+              <Menu.Item key={`/${dataItem.type}/${link.path}`}>
+                <Link to={`/${dataItem.type}/${link.path}`}>{link.label}</Link>
               </Menu.Item>
             ))}
           </Menu.SubMenu>
